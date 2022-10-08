4194754e7f34367c84358f913453372cbb9fea24
spardutti <luisdamian.sp@gmail.com>
2022-10-08 14:51:11 -0300

    sequelize

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 import { config } from "dotenv";
 config();
 
-const sequelize = new Sequelize(process.env.DB_URL as string);
+const sequelize = new Sequelize(process.env.DB_URL as string, {});
 
 export default sequelize;
