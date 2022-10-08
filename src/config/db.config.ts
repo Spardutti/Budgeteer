87a6a0e5cbd497f188de79cf34785a117f313fe4
spardutti <luisdamian.sp@gmail.com>
2022-10-08 12:27:06 -0300

    dialect

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,6 +2,8 @@ import { Sequelize } from "sequelize";
 import { config } from "dotenv";
 config();
 
-const sequelize = new Sequelize(process.env.DB_URL as string);
+const sequelize = new Sequelize(process.env.DB_URL as string, {
+    dialect: "postgres",
+});
 
 export default sequelize;
