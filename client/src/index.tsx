27248d488183e52371fc5f20a6078de45fd7c3f8
spardutti <luisdamian.sp@gmail.com>
2022-10-09 11:47:51 -0300

    Lint

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,11 @@
+import { createRoot } from "react-dom/client";
 
-import ReactDOM from 'react-dom/client';
+import App from "./App";
 
-import App from './App';
+const root = createRoot(document.getElementById("root") as HTMLElement);
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
 root.render(
-  <>
-    <App />
-  </>
+    <>
+        <App />
+    </>
 );
-
-
